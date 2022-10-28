6faeb4a62b2184a65e6095c42e29774a38d6e337
Dranyk <dvdranyk@gmail.com>
2022-10-28 20:20:50 +0300

    1-done, 2-half

--- js/task-01.js
+++ js/task-01.js
@@ -1,31 +1,8 @@
 const categoriesList = document.querySelectorAll('li.item');
 console.log('Number of categories:', categoriesList.length);
-const categories = document.querySelectorAll('h2');
 
-//Animals
-console.log('Category:', categories[0].textContent);
-
-const animalsList = categoriesList[0];
-console.log('Elements:', categoriesList[0].length);
-
-//Products
-console.log('Category:', categories[1].textContent);
-
-const productsList = categoriesList[1];
-console.log('Elements:', categoriesList[0].length);
-
-//Technologies
-console.log('Category:', categories[2].textContent);
-
-const technologiesList = categories[2];
-console.log('Elements:', categoriesList[0].length);
-
-
-//Category: Animals
-//Elements: 4
-
-//Category: Products
-//Elements: 3
-
-//Category: Technologies
-//Elements: 5
+categoriesList.forEach((item) =>{
+console.log(`Category: ${item.firstElementChild.textContent}`);
+const listEl = item.lastElementChild;
+console.log(`Elements: ${listEl.children.length}`);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+const elements = ingredients.map(option => {
+  const item = document.createElement("li");
+  item.textContent = `${ingredients}`;
+  item.classList.add("item");
+
+  return item;
+})
+
+list.append(...elements);
