c66fc9704cdc1da1ced0d5f88a8a38d26fa2087a
Dranyk <dvdranyk@gmail.com>
2022-11-01 16:26:47 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -3,14 +3,14 @@ const form = document.querySelector(".login-form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const {
-    elements: { email, password }
+    elements: { email: { value: email },
+        password: { value: password }, }
   } = event.currentTarget;
 
   if (email.value === "" || password.value === "") {
     return alert("Всі поля мають бути заповнені!");
   }
 
-  console.log(email.value, password.value);
+console.log({ email, password });
   event.currentTarget.reset();
 });
-
